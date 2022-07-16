805b9f0086c83e68f9ba437123fb9ee1a391b27a
nikohonu <nikohonu@gmail.com>
2022-07-16 18:57:11 +0300

    improve ls command

--- ultimate_todo_list/actions/list.py
+++ ultimate_todo_list/actions/list.py
@@ -1,3 +1,4 @@
+import time
 from datetime import date, datetime
 
 from ultimate_todo_list.actions.action import Action
@@ -8,22 +9,218 @@ from ultimate_todo_list.models import (Category, Project, Task, TaskCategory,
 class List(Action):
     def __init__(self, subparsers):
         super().__init__(subparsers, ['ls'], 'list of tasks')
+        list_types = ['tasks', 'projects', 'categories']
+        self.parser.add_argument('-l', '--list', choices=list_types,
+                                 default=list_types[0], help='type of list')
+        sort_types = ['index', 'abc', 'priority', 'interval', 'due', 'completions',
+                      'coefficient', 'score']
+        self.parser.add_argument('-s', '--sort', choices=sort_types,
+                                 default=sort_types[0], help='type of sorting')
+        group_types = [None, 'priority', 'interval', 'due', 'coefficient',
+                       'completions' 'score']
+        self.parser.add_argument('-g', '--group', choices=group_types,
+                                 default=group_types[0], help='type of grouping')
+        self.parser.add_argument('-r', '--reverse', action='store_true')
+
+    def interval_to_int(self, interval):
+        if interval and interval != 'optional':
+            strict = True if interval.startswith('+') else False
+            interval_type = interval[-1]
+            if strict:
+                value = interval[1:-1]
+            else:
+                value = interval[:-1]
+            value = int(value)
+            match interval_type:
+                case 'd':
+                    return value
+                case 'w':
+                    return value*7
+                case 'm':
+                    return value*28
+                case 'y':
+                    return value*365
+        else:
+            return None
+
+    def fix_sort(self, value, reverse):
+        if reverse:
+            return value if value else 0
+        else:
+            return value if value else float('inf')
+
+    def fix_interval(self, interval, reverse):
+        return self.fix_sort(self.interval_to_int(interval), reverse)
+
+    def fix_date(self, date, reverse):
+        date = time.mktime(date.timetuple()) if date else None
+        return self.fix_sort(date, reverse)
+
+    def sort_tasks(self, tasks, sort_type, reverse):
+        match sort_type:
+            case 'index':
+                return sorted(tasks, key=lambda x: x.index, reverse=reverse)
+            case 'abc':
+                return sorted(tasks, key=lambda x: x.name, reverse=reverse)
+            case 'priority':
+                if reverse:
+                    return sorted(tasks, key=lambda x: 9 - x.priority)
+                else:
+                    return sorted(tasks, key=lambda x: 9 - x.priority
+                                  if x.priority > 0 else 0, reverse=True)
+            case 'interval':
+                return sorted(tasks, key=lambda x: self.fix_interval(
+                    x.interval, reverse), reverse=reverse)
+            case 'due':
+                return sorted(tasks, key=lambda x: self.fix_date(x.due, reverse),
+                              reverse=reverse)
+            case 'completions':
+                return sorted(
+                    tasks, key=lambda x: self.fix_sort(
+                        len(x.completions),
+                        reverse),
+                    reverse=reverse)
+            case _:
+                return []
+
+    def group_tasks(self, sort_type, group_type, reverse):
+        tasks = Task.select()
+        match group_type:
+            case None:
+                tasks = self.sort_tasks(tasks, sort_type, reverse)
+                for task in tasks:
+                    print(task)
+            case 'priority':
+                priorities = list(range(1, 9)) + [0] \
+                    if not reverse else reversed(range(0, 9))
+                print(priorities)
+                for priority in priorities:
+                    tasks = self.sort_tasks(tasks, sort_type, reverse)
+                    tmp_tasks = list(filter(lambda x: x.priority == priority, tasks))
+                    if tmp_tasks:
+                        result = f'Priority: {priority}'
+                        print(f'{result}\n{"=" * len(result)}')
+                        for task in tmp_tasks:
+                            print(task)
+                        print()
+            # TODO: group by interval
+            case 'interval':
+                def print_interval(interval):
+                    t = ''
+                    if interval % 365 == 0:
+                        v = int(interval / 365)
+                        t = 'year'
+                    elif interval % 28 == 0:
+                        v = int(interval / 28)
+                        t = 'month'
+                    elif interval % 7 == 0:
+                        v = int(interval / 7)
+                        t = 'week'
+                    else:
+                        v = interval
+                        t = 'day'
+                    v = f' {v}' if v > 1 else ''
+                    t = t + 's' if v != '' else t
+                    result = f'Recurence: every{v} {t}'
+                    print(f'{result}\n{"=" * len(result)}')
+                tasks = self.sort_tasks(tasks, 'interval', reverse)
+                intervals = sorted(set([self.interval_to_int(task.interval)
+                                        for task in tasks
+                                        if self.interval_to_int(task.interval)]))
+                for interval in intervals:
+                    print_interval(interval)
+                    for task in filter(lambda x: self.interval_to_int(x.interval) ==
+                                       interval, tasks):
+                        print(task)
+                    print()
+            case 'due':
+                def print_delta(delta):
+                    result = 'Due: today'
+                    v = abs(delta[0]) if abs(delta[0]) > 1 else 'a'
+                    t = ''
+                    match delta[1]:
+                        case 'd':
+                            t = 'day'
+                        case 'w':
+                            t = 'week'
+                        case 'm':
+                            t = 'month'
+                        case 'y':
+                            t = 'year'
+                    t = t + 's' if v != 'a' else t
+                    if delta[0] > 0:
+                        result = f'Due: in {v} {t}'
+                    elif delta[0] < 0:
+                        result = f'Due: {v} {t} ago'
+                    print(f'{result}\n{"=" * len(result)}')
+
+                tasks = self.sort_tasks(tasks, 'due', reverse)
+                tasks = filter(lambda x: x.due != None, tasks)
+                now = datetime.now().date()
+                deltas = {}
+                for task in tasks:
+                    days = (task.due-now).days
+                    years = int(days / 365.25)
+                    months = int(days / (365.25/12))
+                    weeks = int(days / 7)
+                    if abs(years) >= 1:
+                        key = (years, 'y')
+                        if key not in deltas:
+                            deltas[key] = [task]
+                        else:
+                            deltas[key].append(task)
+                    elif abs(months) >= 1:
+                        key = (months, 'm')
+                        if key not in deltas:
+                            deltas[key] = [task]
+                        else:
+                            deltas[key].append(task)
+                    elif abs(weeks) >= 1:
+                        key = (weeks, 'w')
+                        if key not in deltas:
+                            deltas[key] = [task]
+                        else:
+                            deltas[key].append(task)
+                    else:
+                        key = (days, 'd')
+                        if key not in deltas:
+                            deltas[key] = [task]
+                        else:
+                            deltas[key].append(task)
+                for delta in deltas:
+                    print_delta(delta)
+                    tasks = self.sort_tasks(deltas[delta], sort_type, reverse)
+                    for task in tasks:
+                        print(task)
+                    print()
+
+    def sort_projects_or_categories(self, entities, sort_type, reverse):
+        match sort_type:
+            case ('abc' | 'index'):
+                return sorted(entities, key=lambda x: x.name, reverse=reverse)
+            case 'coefficient':
+                return sorted(entities, key=lambda x: x.coefficient, reverse=reverse)
+            case 'score':
+                return sorted(entities, key=lambda x: x.score, reverse=reverse)
+            case _:
+                return []
+
+    def group_categories(self, sort_type, group_type, reverse):
+        categories = Category.select()
+        match group_type:
+            case None:
+                categories = self.sort_projects_or_categories(
+                    categories, sort_type, reverse)
+                for category in categories:
+                    print(category)
+            case '':
+                pass
 
     def do(self, args):
-        tasks = Task.select()
-        dates = sorted(set([task.due for task in tasks if task.due != None]))
-        for date in dates:
-            print(date)
-            local_tasks = [task for task in tasks if task.due == date]
-            local_tasks = sorted(local_tasks, key=lambda x: x.priority)
-            for task in local_tasks:
-                if task.option != 'deleted':
-                    print(task)
-        print(None)
-        local_tasks = [task for task in tasks if task.due == None]
-        local_tasks = sorted(local_tasks, key=lambda x: x.priority)
-        for task in local_tasks:
-            if task.option != 'deleted':
-                print(task)
-
-        print(args)
+        match args.list:
+            case 'tasks':
+                self.group_tasks(args.sort, args.group, args.reverse)
+            case 'projects':
+                pass
+            case 'categories':
+                self.group_categories(args.sort, args.group, args.reverse)
--- ultimate_todo_list/models.py
+++ ultimate_todo_list/models.py
@@ -9,6 +9,11 @@ data_dir = Path(user_data_dir('ultimate_todo_list', 'nikohonu'))
 data_dir.mkdir(parents=True, exist_ok=True)
 db = SqliteDatabase(data_dir / 'ultimate_todo_list.db')
 
+def cyan_str(string):
+    return f'\033[36m{string}\033[00m'
+
+def red_str(string):
+    return f'\033[91m{string}\033[00m'
 
 class BaseModel(Model):
     '''Base model for table in database'''
@@ -24,7 +29,7 @@ class Task(BaseModel):
     due = DateField(null=True)
     option = TextField(null=True)  # autoFail, autoComplete, hidden, deleted
 
-    priorities = set(range(1, 9)) | {0}
+    priorities = list(range(1, 9)) + [0]
     options = {'autoFail', 'autoComplete', 'hidden', None}
 
     @property
@@ -55,70 +60,90 @@ class Task(BaseModel):
         return Task.get(index=index)
 
     def __str__(self):
-        max_index=len(str(max(Task.get_indexes())))
-        projects=''
-        categories=''
+        max_index = len(str(max(Task.get_indexes())))
+        projects = ''
+        categories = ''
         for project in self.projects:
             projects += f' +{project.name}'
         for category in self.categories:
             categories += f' @{category.name}'
-        priority=f' ({self.priority})' if self.priority else ''
-        rec=''
+        priority = f' ({self.priority})' if self.priority else ''
+        rec = ''
         if self.interval:
-            rec=f' rec:{self.interval}'
-        due=''
+            rec = f' rec:{self.interval}'
+        due = ''
         if self.due:
-            due=f' due:{self.due}'
-        option=''
+            due = f' due:{self.due}'
+        option = ''
+        completion = f' completions:{len(self.completions)}'\
+            if self.completions else ''
         if self.option:
-            option=f' option:{self.option}'
-        return f'|{self.index:{max_index}}|{priority} {self.name}{projects}{categories}{rec}{due}{option}'
+            option = f' option:{self.option}'
+        return f'|{self.index:{max_index}}|{priority} {self.name}{projects}{categories}{rec}{due}{option}{completion}'
 
 
 class Project(BaseModel):
-    coefficient=FloatField(default=1)
-    name=TextField(unique=True)
-    option=TextField(null=True)  # hidden, deleted
+    coefficient = FloatField(default=1)
+    name = TextField(unique=True)
+    option = TextField(null=True)  # hidden, deleted
 
     def __str__(self):
-        return self.name
+        coefficient = f'({self.coefficient}) ' if self.coefficient else ''
+        return f'{coefficient}{self.name}'
 
 
 class Category(BaseModel):
-    coefficient=FloatField(default=1)
-    name=TextField(unique=True)
-    option=TextField(null=True)  # hidden, deleted
+    coefficient = FloatField(default=1)
+    name = TextField(unique=True)
+    option = TextField(null=True)  # hidden, deleted
+
+    @property
+    def completions(self):
+        tasks = Task.select()
+        result = []
+        for task in tasks:
+            if self in task.categories:
+                result += list(task.completions)
+        return result
+
+    @property
+    def score(self):
+        return len(self.completions) / self.coefficient
+
 
     def __str__(self):
-        return self.name
+        coefficient = f'({self.coefficient}) ' if self.coefficient else ''
+        completions = f' completions:{len(self.completions)}' if self.completions else ''
+        score = f' {cyan_str("score")}:{red_str(self.score)}' if self.score else ''
+        return f'{coefficient}{self.name}{completions}{score}'
 
 
 class TaskProject(BaseModel):
-    task=ForeignKeyField(Task, on_delete='CASCADE', backref='task_projects')
-    project=ForeignKeyField(Project, on_delete='CASCADE',
+    task = ForeignKeyField(Task, on_delete='CASCADE', backref='task_projects')
+    project = ForeignKeyField(Project, on_delete='CASCADE',
                               backref='project_tasks')
 
     class Meta:
-        primary_key=CompositeKey('task', 'project')
+        primary_key = CompositeKey('task', 'project')
 
 
 class TaskCategory(BaseModel):
-    task=ForeignKeyField(Task, on_delete='CASCADE',
+    task = ForeignKeyField(Task, on_delete='CASCADE',
                            backref='task_categories')
-    category=ForeignKeyField(Category, on_delete='CASCADE',
+    category = ForeignKeyField(Category, on_delete='CASCADE',
                                backref='category_tasks')
 
     class Meta:
-        primary_key=CompositeKey('task', 'category')
+        primary_key = CompositeKey('task', 'category')
 
 
 class Completion(BaseModel):
-    task=ForeignKeyField(Task, on_delete='CASCADE', backref='completion')
-    date=DateField()
+    task = ForeignKeyField(Task, on_delete='CASCADE', backref='completions')
+    date = DateField()
 
     class Meta:
-        primary_key=CompositeKey('task', 'date')
+        primary_key = CompositeKey('task', 'date')
 
 
-models=BaseModel.__subclasses__()
+models = BaseModel.__subclasses__()
 db.create_tables(models)
